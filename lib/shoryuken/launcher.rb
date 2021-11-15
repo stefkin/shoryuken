71dd25d7341bcaf7f6f7ab9ddd9c8fd203fcfa98
Usevalad Stsefkin <884211+stefkin@users.noreply.github.com>
2021-11-15 17:20:44 +0100

    use concurrency var properly

--- lib/shoryuken/launcher.rb
+++ lib/shoryuken/launcher.rb
@@ -82,7 +82,7 @@ module Shoryuken
           group,
           Shoryuken::Fetcher.new(group),
           Shoryuken.polling_strategy(group).new(options[:queues], Shoryuken.delay(group)),
-          Shoryuken.groups['default'][:concurrency],
+          Shoryuken.options[:concurrency],
           executor,
           @busy_processors
         )
